d7d157122186e5b48c173fb60c078403da4424de
paciadawid <paciadawid@gmail.com>
2024-06-05 11:15:45 +0300

    appium_by

--- test_run.py
+++ test_run.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from appium import webdriver
+
 from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
 
 app_name = 'calculator.apk'
 # path = os.path.join(os.path.abspath(__file__).split(), app_name)
@@ -10,9 +12,16 @@ path = str(Path(__file__).parent.joinpath(app_name))
 appium_url = 'http://127.0.0.1:4723'
 
 capabilities = dict(
-    automationName = 'uiautomator2',
-    platformName = 'Android',
-    app = path
+    automationName='uiautomator2',
+    platformName='Android',
+    app=path
 )
 
 driver = webdriver.Remote(appium_url, options=UiAutomator2Options().load_capabilities(capabilities))
+
+
+button_1 = driver.find_element(AppiumBy.ID, 'formula_container')
+# button_1_ = driver.find_elements(AppiumBy.ACCESSIBILITY_ID, '1')
+
+
+
